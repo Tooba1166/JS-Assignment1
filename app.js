d2ea45562ea0964e3e6cfca9d424baec7b041e74
Tooba1166 <163329886+Tooba1166@users.noreply.github.com>
2024-05-25 22:11:33 +0500

    chap 12 and 13

--- app.js
+++ app.js
@@ -1,182 +1,143 @@
-// let city = prompt("Enter your city name:");
-// if (city.toLowerCase() === "karachi") {
-//     alert("Welcome to city of lights");
+// let studentNamesLiteral = [];
+
+
+// let studentNamesObject = new Array();
+
+
+// let stringsArray = ["apple", "banana", "cherry"];
+
+
+// let numbersArray = [1, 2, 3, 4, 5];
+
+
+// let booleanArray = [true, false, true, false];
+
+
+// let mixedArray = [1, "two", true, null, undefined];
+
+
+// let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+// document.write("<h1>Qualifications:</h1><ul>");
+// for (let i = 0; i < qualifications.length; i++) {
+//     document.write("<li>" + qualifications[i] + "</li>");
+// }
+// document.write("</ul>");
+
+
+// let studentNames = ["Michael", "John", "Tony"];
+// let studentScores = [320, 230, 480];
+// let totalMarks = 500;
+
+// for (let i = 0; i < studentNames.length; i++) {
+//     let percentage = (studentScores[i] / totalMarks) * 100;
+//     document.write("Score of " + studentNames[i] + " is " + studentScores[i] + ". Percentage: " + percentage + "%<br>");
+// }
+
+
+// let colors = ["Red", "Green", "Blue"];
+// document.write("<h1>Initial Colors:</h1> " + colors.join(", ") + "<br>");
+
+// a. Ask the user what color he/she wants to add to the beginning & add that color to the beginning of the array.
+// let colorToAddBeginning = prompt("Enter a color to add to the beginning:");
+// colors.unshift(colorToAddBeginning);
+// document.write("<h1>After adding color to the beginning:</h1> " + colors.join(", ") + "<br>");
+
+// b. Ask the user what color he/she wants to add to the end & add that color to the end of the array.
+// let colorToAddEnd = prompt("Enter a color to add to the end:");
+// colors.push(colorToAddEnd);
+// document.write("<h1>After adding color to the end:</h1> " + colors.join(", ") + "<br>");
+
+// c. Add two more colors to the beginning of the array.
+// colors.unshift("Yellow", "Purple");
+// document.write("<h1>After adding two more colors to the beginning:</h1> " + colors.join(", ") + "<br>");
+
+// d. Delete the first color in the array.
+// colors.shift();
+// document.write("<h1>After deleting the first color:</h1> " + colors.join(", ") + "<br>");
+
+// e. Delete the last color in the array.
+// colors.pop();
+// document.write("<h1>After deleting the last color:</h1> " + colors.join(", ") + "<br>");
+
+// f. Ask the user at which index he/she wants to add a color & color name. Then add the color to desired position/index.
+// let indexToAdd = parseInt(prompt("Enter the index at which you want to add a color:"));
+// let colorToAdd = prompt("Enter the color to add:");
+// colors.splice(indexToAdd, 0, colorToAdd);
+// document.write("<h1>After adding color at index " + indexToAdd + ":</h1> " + colors.join(", ") + "<br>");
+
+// g. Ask the user at which index he/she wants to delete color(s) & how many colors he/she wants to delete. Then remove the same number of color(s) from user-defined position/index.
+// let indexToDelete = parseInt(prompt("Enter the index from which you want to delete color(s):"));
+// let numOfColorsToDelete = parseInt(prompt("Enter the number of colors to delete:"));
+// colors.splice(indexToDelete, numOfColorsToDelete);
+// document.write("<h1>After deleting color(s) from index " + indexToDelete + ":</h1> " + colors.join(", ") + "<br>");
+
+
+// let scores = [320, 145, 480, 120];
+// document.write("Scores of Students: " + scores.join(", ") + "<br>");
+// scores.sort(function(a, b) { return a - b; });
+// document.write("Ordered Scores of Students: " + scores.join(", ") + "<br>");
+
+
+// let cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
+// document.write("<h1>Cities list:</h1> " + cities.join(", ") + "<br>");
+
+// let selectedCities = cities.slice(2, 4); // Copies elements from index 2 to index 4 (Islamabad, Quetta, Peshawar)
+// document.write("<h1>Selected cities list:</h1> " + selectedCities.join(", ") + "<br>");
+
+
+
+// let arr = ["This", " is", " my", " cat"];
+// let singleString = arr.join("");
+// document.write("Array: " + arr.join(",") + "<br>");
+// document.write("String: " + singleString + "<br>");
+
+
+// let devices = ["keyboard", "printer", "mouse", "monitor"];
+// document.write("<h1>Devices:</h1> " + devices.join(", ") + "<br>");
+
+// while (devices.length > 0) {
+//     document.write("Out: " + devices.shift() + "<br>");
 // }
 
 
 
-// let gender = prompt("Enter your gender (male/female):");
-// if (gender.toLowerCase() === "male") {
-//     alert("Good Morning Sir");
-// } else if (gender.toLowerCase() === "female") {
-//     alert("Good Morning Ma’am");
+// let devices = [];
+
+// Function to push elements to the stack
+// function pushToStack(element) {
+//     devices.push(element);
 // }
 
-
-
-// let signalColor = prompt("Enter the traffic signal color (Red/Yellow/Green):").toLowerCase();
-// if (signalColor === "red") {
-//     alert("Must Stop");
-// } else if (signalColor === "yellow") {
-//     alert("Ready to move");
-// } else if (signalColor === "green") {
-//     alert("Move now");
+// Function to pop elements from the stack and display
+// function displayStack() {
+//     document.write("<h1>Devices in LIFO order:</h1>");
+//     while (devices.length > 0) {
+//         document.write("Out: " + devices.pop() + "<br>");
+//     }
 // }
 
+// Storing values in LIFO order
+// pushToStack("keyboard");
+// pushToStack("mouse");
+// pushToStack("printer");
+// pushToStack("monitor");
 
-
-// let fuel = parseFloat(prompt("Enter remaining fuel in car (in litres):"));
-// if (fuel < 0.25) {
-//     alert("Please refill the fuel in your car");
-// }
-
-
-
-
-// a
-// var a = 4;
-// if (++a === 5) {
-//     alert("given condition for variable a is true");
-// }
-
-// // b
-// var b = 82;
-// if (b++ === 83) {
-//     alert("given condition for variable b is true");
-// }
-
-// // c
-// var c = 12;
-// if (c++ === 13) {
-//     alert("condition 1 is true");
-// }
-// if (c === 13) {
-//     alert("condition 2 is true");
-// }
-// if (++c < 14) {
-//     alert("condition 3 is true");
-// }
-// if (c === 14) {
-//     alert("condition 4 is true");
-// }
-
-// // d
-// var materialCost = 20000;
-// var laborCost = 2000;
-// var totalCost = materialCost + laborCost;
-// if (totalCost === laborCost + materialCost) {
-//     alert("The cost equals");
-// }
-
-// // e
-// if (true) {
-//     alert("True");
-// }
-// if (false) {
-//     alert("False");
-// }
-
-// // f
-// if ("car" < "cat") {
-//     alert("car is smaller than cat");
-// }
+// Displaying values in LIFO order
+// displayStack();
 
 
 
-// let subject1 = parseInt(prompt("Enter marks obtained in first subject:"));
-// let subject2 = parseInt(prompt("Enter marks obtained in second subject:"));
-// let subject3 = parseInt(prompt("Enter marks obtained in third subject:"));
-// let totalMarks = parseInt(prompt("Enter total marks:"));
+let phoneManufacturers = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Haier"];
 
-// let marksObtained = subject1 + subject2 + subject3;
-// let percentage = (marksObtained / totalMarks) * 100;
+document.write("<h1>Select Phone Manufacturer:</h1>");
+document.write("<select>");
 
-// let grade, remarks;
-
-// if (percentage >= 80) {
-//     grade = "A-one";
-//     remarks = "Excellent";
-// } else if (percentage >= 70) {
-//     grade = "A";
-//     remarks = "Good";
-// } else if (percentage >= 60) {
-//     grade = "B";
-//     remarks = "You need to improve";
-// } else {
-//     grade = "Fail";
-//     remarks = "Sorry";
-// }
-
-// alert(`Marks Sheet\nTotal marks: ${totalMarks}\nMarks obtained: ${marksObtained}\nPercentage: ${percentage}%\nGrade: ${grade}\nRemarks: ${remarks}`);
-
-
-
-// let secretNumber = Math.floor(Math.random() * 10) + 1;
-// let userGuess = parseInt(prompt("Guess the secret number (1-10):"));
-
-// if (userGuess === secretNumber) {
-//     alert("Bingo! Correct answer");
-// } else if (userGuess + 1 === secretNumber) {
-//     alert("Close enough to the correct answer");
-// }
-
-
-// let number = parseInt(prompt("Enter a number:"));
-// if (number % 3 === 0) {
-//     alert("The number is divisible by 3");
-// } else {
-//     alert("The number is not divisible by 3");
-// }
-
-
-
-
-
-
-// let num = parseInt(prompt("Enter a number:"));
-// if (num % 2 === 0) {
-//     alert("The number is even");
-// } else {
-//     alert("The number is odd");
-// }
-
-
-
-
-// let temperature = parseInt(prompt("Enter the temperature:"));
-// if (temperature > 40) {
-//     alert("It is too hot outside.");
-// } else if (temperature > 30) {
-//     alert("The Weather today is Normal.");
-// } else if (temperature > 20) {
-//     alert("Today’s Weather is cool.");
-// } else if (temperature > 10) {
-//     alert("OMG! Today’s weather is so Cool.");
-// }
-
-
-
-
-
-let firstNumber = parseFloat(prompt("Enter the first number:"));
-let secondNumber = parseFloat(prompt("Enter the second number:"));
-let operation = prompt("Enter the operation (+, -, *, /, %):");
-
-let result;
-
-if (operation === "+") {
-    result = firstNumber + secondNumber;
-} else if (operation === "-") {
-    result = firstNumber - secondNumber;
-} else if (operation === "*") {
-    result = firstNumber * secondNumber;
-} else if (operation === "/") {
-    result = firstNumber / secondNumber;
-} else if (operation === "%") {
-    result = firstNumber % secondNumber;
-} else {
-    result = "Invalid operation";
+for (let i = 0; i < phoneManufacturers.length; i++) {
+    document.write("<option value='" + phoneManufacturers[i] + "'>" + phoneManufacturers[i] + "</option>");
 }
 
-alert("The result is: " + result);
+document.write("</select>");
+
+
+
 
