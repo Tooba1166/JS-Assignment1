9448f4da8021d59a3849754acb5e2d872c5151c2
Tooba1166 <163329886+Tooba1166@users.noreply.github.com>
2024-06-07 14:37:34 +0500

    Chap 21-25

--- app.js
+++ app.js
@@ -1,118 +1,119 @@
-// let multiArray = [[]];
+// let firstName = prompt("Enter your first name:");
+// let lastName = prompt("Enter your last name:");
+// let fullName = firstName + " " + lastName;
+// alert("Hello, " + fullName + "!");
 
 
-// let matrix = [
-//     [0, 1, 2, 3],
-//     [1, 0, 1, 2],
-//     [2, 1, 0, 1]
-// ];
+// let favoritePhone = prompt("Enter your favorite mobile phone model:");
+// document.write("My favorite phone is: " + favoritePhone + "<br>");
+// document.write("Length of string: " + favoritePhone.length);
 
 
-// for (let i = 1; i <= 10; i++) {
-//     document.write(i + "<br>");
+// let word = "Pakistani";
+// let index = word.indexOf("n");
+// document.write("String: " + word + "<br>");
+// document.write("Index of 'n': " + index);
+
+
+// let word = "Hello World";
+// let lastIndex = word.lastIndexOf("l");
+// document.write("String: " + word + "<br>");
+// document.write("Last index of 'l': " + lastIndex);
+
+
+// let word = "Pakistani";
+// let character = word.charAt(3);
+// document.write("String: " + word + "<br>");
+// document.write("Character at index 3: " + character);
+
+
+// let firstName = prompt("Enter your first name:");
+// let lastName = prompt("Enter your last name:");
+// let fullName = firstName.concat(" ", lastName);
+// alert("Hello, " + fullName + "!");
+
+
+// let city = "Hyderabad";
+// let newCity = city.replace("Hyder", "Islam");
+// document.write("City: " + city + "<br>");
+// document.write("After replacement: " + newCity);
+
+
+// let message = "Ali and Sami are best friends. They play cricket and football together.";
+// let newMessage = message.replace(/and/g, "&");
+// document.write(newMessage);
+
+
+// let str = "472";
+// let num = Number(str);
+// document.write("Value: " + str + "<br>");
+// document.write("Type: " + typeof str + "<br>");
+// document.write("Value: " + num + "<br>");
+// document.write("Type: " + typeof num);
+
+
+// let userInput = prompt("Enter a word:");
+// let upperCaseInput = userInput.toUpperCase();
+// document.write("User input: " + userInput + "<br>");
+// document.write("Upper case: " + upperCaseInput);
+
+
+// let userInput = prompt("Enter a word:");
+// let titleCaseInput = userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+// document.write("User input: " + userInput + "<br>");
+// document.write("Title case: " + titleCaseInput);
+
+
+// let num = 35.36;
+// let str = num.toString().replace(".", "");
+// document.write("Number: " + num + "<br>");
+// document.write("Result: " + str);
+
+
+// let username = prompt("Enter a username:");
+// let invalidChars = /[!@.,]/;
+// while (invalidChars.test(username)) {
+//     alert("Please enter a valid username.");
+//     username = prompt("Enter a username:");
 // }
-
-
-// let tableNumber = parseInt(prompt("Enter the table number:"));
-// let tableLength = parseInt(prompt("Enter the length of the table:"));
-
-// for (let i = 1; i <= tableLength; i++) {
-//     document.write(tableNumber + " x " + i + " = " + (tableNumber * i) + "<br>");
-// }
-
-
-// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
-// for (let i = 0; i < fruits.length; i++) {
-//     document.write(fruits[i] + "<br>");
-// }
-
-// for (let i = 0; i < fruits.length; i++) {
-//     document.write("Element at index " + i + " is " + fruits[i] + "<br>");
-// }
-
-
-// a. Counting
-// document.write("Counting:<br>");
-// for (let i = 1; i <= 15; i++) {
-//     document.write(i + ", ");
-// }
-// document.write("<br><br>");
-
-// b. Reverse counting
-// document.write("Reverse counting:<br>");
-// for (let i = 10; i >= 1; i--) {
-//     document.write(i + ", ");
-// }
-// document.write("<br><br>");
-
-// c. Even
-// document.write("Even:<br>");
-// for (let i = 0; i <= 20; i += 2) {
-//     document.write(i + ", ");
-// }
-// document.write("<br><br>");
-
-// d. Odd
-// document.write("Odd:<br>");
-// for (let i = 1; i < 20; i += 2) {
-//     document.write(i + ", ");
-// }
-// document.write("<br><br>");
-
-// e. Series
-// document.write("Series:<br>");
-// for (let i = 2; i <= 20; i += 2) {
-//     document.write(i + "k, ");
-// }
-// document.write("<br><br>");
-
+// alert("Username is valid.");
 
 
 // let A = ["cake", "apple pie", "cookie", "chips", "patties"];
-// let searchItem = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?");
-
-// let found = false;
-// for (let i = 0; i < A.length; i++) {
-//     if (A[i].toLowerCase() === searchItem.toLowerCase()) {
-//         document.write(searchItem + " is available at index " + i + " in our bakery<br>");
-//         found = true;
-//         break;
-//     }
-// }
-// if (!found) {
-//     document.write("We are sorry. " + searchItem + " is not available in our bakery<br>");
+// let userInput = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?").toLowerCase();
+// let index = A.indexOf(userInput);
+// if (index !== -1) {
+//     alert(userInput + " is available at index " + index + " in our bakery");
+// } else {
+//     alert("We are sorry. " + userInput + " is not available in our bakery");
 // }
 
 
-// let A = [24, 53, 78, 98, 12];
-// let largestNumber = A[0];
+// let password = prompt("Enter a password:");
+// let isValid = /^(?!\d)[A-Za-z\d]{6,}$/.test(password);
+// while (!isValid) {
+//     alert("Password must contain alphabets and numbers, should not start with a number, and must be at least 6 characters long.");
+//     password = prompt("Enter a password:");
+//     isValid = /^(?!\d)[A-Za-z\d]{6,}$/.test(password);
+// }
+// alert("Password is valid.");
 
-// for (let i = 1; i < A.length; i++) {
-//     if (A[i] > largestNumber) {
-//         largestNumber = A[i];
-//     }
+
+// let university = "University of Karachi";
+// let array = university.split("");
+// for (let i = 0; i < array.length; i++) {
+//     document.write(array[i] + "<br>");
 // }
 
-// document.write("Array items: " + A.join(", ") + "<br>");
-// document.write("The largest number is " + largestNumber + "<br>");
+
+// let userInput = prompt("Enter a word:");
+// let lastChar = userInput.charAt(userInput.length - 1);
+// document.write("User input: " + userInput + "<br>");
+// document.write("Last character of input: " + lastChar);
 
 
-// let A = [24, 53, 78, 91, 9];
-// let smallestNumber = A[0];
-
-// for (let i = 1; i < A.length; i++) {
-//     if (A[i] < smallestNumber) {
-//         smallestNumber = A[i];
-//     }
-// }
-
-// document.write("Array items: " + A.join(", ") + "<br>");
-// document.write("The smallest number is " + smallestNumber + "<br>");
-
-
-
-for (let i = 5; i <= 100; i += 5) {
-    document.write(i + ", ");
-}
-
-
+let text = "The quick brown fox jumps over the lazy dog";
+        let word = "the";
+        let count = (text.toLowerCase().match(/the/g) || []).length;
+        document.write("Text: " + text + "<br>");
+        document.write("There are " + count + " occurrence(s) of word 'the'");
